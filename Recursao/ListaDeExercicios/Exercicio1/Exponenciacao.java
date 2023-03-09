fd73f6bf4f12dca0de89cbb16385881c948d7b07
Nasc1mento <reisadryan16@gmail.com>
2023-03-09 20:50:50 -0300

    adicionado o exercício 1 da lista de exercício sobre recursão

---
+++ Recursao/ListaDeExercicios/Exercicio1/Exponenciacao.java
@@ -0,0 +1,29 @@
+
+/*
+ * Instituto Federal de Educação, Ciência e Tecnologia de Pernambuco - Campus Igarassu
+ * Componente Curricular: Algorítmos e Estrutura de Dados
+ * Professor: Milton Secundino
+ * Aluno: Adryan Nascimento Reis 
+ * Exercício 01 - Implemente uma função recursiva que, dados dois números inteiros x e n, calcula o valor de xn
+ * 
+ * 
+ */
+
+package Recursao.ListaDeExercicios.Exercicio1;
+
+public class Exponenciacao {
+	
+	public static int expoenenciacao(int x, int n) {
+		if (n <= 1) {
+			return x;
+		}
+		
+		return x*expoenenciacao(x, n-1);
+	}
+	
+	public static void main(String[] args) {
+		int resposta = expoenenciacao(3, 4);
+		System.out.println(resposta);
+	}
+}
+
