89b38d760e3131307e8d827d23e650afcae11e9d
Adryan N. Reis <88512599+Nasc1mento@users.noreply.github.com>
2023-03-31 11:00:12 -0300

    Add files via upload

---
+++ stack/Main.java
@@ -0,0 +1,18 @@
+package stack;
+
+public class Main {
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		stack.push(40);
+		stack.push(50);
+		System.out.println(stack);
+		System.out.println(stack.peek());
+		stack.pop();
+		stack.pop();
+		System.out.println(stack);
+		System.out.println(stack.peek());
+	}
+}
