b2b9a5aad13aea1f67c6413f10daeb338012d5d2
Nasc1mento <reisadryan16@gmail.com>
2023-04-01 23:39:01 -0300

    bubble sort

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -3,6 +3,28 @@ package sort;
 public class BubbleSort {
 
 	public static int[] bubbleSort(int[] arr) {
-		return null;
+		boolean sorted = false;
+		while(!sorted) {
+			sorted = true;
+			for (int i = 1; i < arr.length; i++) {
+				if (arr[i] < arr[i-1]) {
+					int temp = arr[i];
+					arr[i] = arr[i-1];
+					arr[i-1] = temp;
+					sorted = false;
+				}
+			}
+		}
+				
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {-2, -3, -5, 10, -1, -10, 50};
+		bubbleSort(arr);
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
 	}
 }
--- sort/InsertionSort.java
+++ sort/InsertionSort.java
@@ -2,9 +2,9 @@ package sort;
 
 public class InsertionSort {
 
-	public static int[] insertionSort(int[] arr) {		
-		for (int i = 1; i < arr.length; i++) {			
-			for(int j = i; j > 0; j--) {				
+	public static int[] insertionSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			for(int j = i; j > 0; j--) {
 				if (arr[j] < arr[j-1]) {
 					int temp = arr[j];
 					arr[j] = arr[j-1];
