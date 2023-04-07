f5b9a7870ebb4e428a54824f930eaeeaa424c89f
Nasc1mento <reisadryan16@gmail.com>
2023-04-07 01:53:44 -0300

    adicionado exercício de bubble sort e arquivos de entrada e saída(output100.txt não será enviado pois ultrapassa os 100mb)

--- sort/bubble/BubbleSort.java
+++ sort/bubble/BubbleSort.java
@@ -1,5 +1,9 @@
 package sort.bubble;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class BubbleSort {
 
 	public static int[] bubbleSort(int[] arr) {
@@ -14,15 +18,39 @@ public class BubbleSort {
 					sorted = false;
 				}
 			}
+			if (!sorted)
+				printArray(arr);
 		}				
 		return arr;
 	}
 	
+	public static void printArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+	        if (i < arr.length - 1) {
+	            System.out.print(" ");
+	        }
+		}
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
-		int[] arr = {-2, -3, -5, 10, -1, -10, 50};
-		bubbleSort(arr);
-		for(int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
+		try {
+			BufferedReader myreader = new BufferedReader(new FileReader("sort/bubble/input100.txt"));
+			String line = myreader.readLine();
+			int [] array;
+			while (line != null) {
+				String[] num = (line.split(" "));
+				array = new int[Integer.parseInt(num[1])];			
+				for (int i = 0; i < array.length; i++) {
+					array[i] = Integer.parseInt(num[2+i]);
+				}			
+				bubbleSort(array);				
+				line = myreader.readLine();
+			}
+			myreader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
 	}
