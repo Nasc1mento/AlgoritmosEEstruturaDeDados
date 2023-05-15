8b49a9c073323ce57647609f031399c83135e20e
Adryan N. Reis <88512599+Nasc1mento@users.noreply.github.com>
2023-05-15 09:57:26 -0300

    Update Dealership.java

--- test/unit/first/question3/Dealership.java
+++ test/unit/first/question3/Dealership.java
@@ -1,19 +1,26 @@
 package test.unit.first.question3;
 
 public class Dealership {
-	
+
 	private Queue queueCarSale;
 	private Queue queueMechanicalWorkshop;
 	private Queue queueAccessories;
-	private Funcionary receptionist;
-	
+	private Receptionist receptionist;
+
 	public Dealership() {
 		this.queueCarSale = new Queue(Departaments.CAR_SALE);
 		this.queueMechanicalWorkshop = new Queue(Departaments.MECHANICAL_WORKSHOP);
 		this.queueAccessories = new Queue(Departaments.ACCESSORIES);
 		this.receptionist = new Receptionist("Fulano");
 	}
-	
-	
-	
+
+	public void enter(Client client) {
+		Departaments deparament = receptionist.redirect();
+		if (deparament == Departaments.ACCESSORIES)
+			queueAccessories.enqueue(client);
+		else if (deparament == Departaments.CAR_SALE)
+			queueCarSale.enqueue(client);
+		else if (deparament == Departaments.MECHANICAL_WORKSHOP)
+			queueMechanicalWorkshop.enqueue(client);
+	}
 }
