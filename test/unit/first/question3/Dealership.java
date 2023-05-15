c932ca16e8ccaee18b282b743db03e504fc41f38
Adryan N. Reis <88512599+Nasc1mento@users.noreply.github.com>
2023-05-15 10:38:49 -0300

    Update Dealership.java

--- test/unit/first/question3/Dealership.java
+++ test/unit/first/question3/Dealership.java
@@ -6,12 +6,18 @@ public class Dealership {
 	private Queue queueMechanicalWorkshop;
 	private Queue queueAccessories;
 	private Receptionist receptionist;
+	private Attendant attendantCarSale;
+	private Attendant attendantMechanicalWorkshop;
+	private Attendant attendantAcessories;
 
 	public Dealership() {
 		this.queueCarSale = new Queue(Departaments.CAR_SALE);
 		this.queueMechanicalWorkshop = new Queue(Departaments.MECHANICAL_WORKSHOP);
 		this.queueAccessories = new Queue(Departaments.ACCESSORIES);
 		this.receptionist = new Receptionist("Fulano");
+		this.attendantAcessories = new Attendant("Fulano Atendente Acessorios");
+		this.attendantCarSale = new Attendant("Fulano Atendente Carros");
+		this.attendantAcessories = new Attendant("Fulano Atendente Mecanico");
 	}
 
 	public void enter(Client client) {
@@ -22,5 +28,11 @@ public class Dealership {
 			queueCarSale.enqueue(client);
 		else if (deparament == Departaments.MECHANICAL_WORKSHOP)
 			queueMechanicalWorkshop.enqueue(client);
+	}
+	
+	public void serve() {
+		attendantAcessories.serve(queueAccessories);
+		attendantCarSale.serve(queueCarSale);
+		attendantMechanicalWorkshop.serve(queueMechanicalWorkshop);
 	}
 }
