728d3499a28ca51be7d89b39c7a00511a36b3362
Adryan N. Reis <88512599+Nasc1mento@users.noreply.github.com>
2023-05-18 10:08:38 -0300

    Create NodeClient.java

---
+++ test/unit/first/question3/NodeClient.java
@@ -0,0 +1,34 @@
+package test.unit.first.question3;
+
+public class NodeClient {
+	
+	
+	private NodeClient next;
+	private Client client;
+	
+	
+	public NodeClient(NodeClient next, Client client) {
+		this.next = next;
+		this.client = client;
+	}
+	
+	public NodeClient(Client client) {
+		this.next = null;
+		this.client = client;
+	}
+
+
+	public NodeClient getNext() {
+		return next;
+	}
+
+
+	public void setNext(NodeClient next) {
+		this.next = next;
+	}
+
+
+	public Client getClient() {
+		return client;
+	}
+}
