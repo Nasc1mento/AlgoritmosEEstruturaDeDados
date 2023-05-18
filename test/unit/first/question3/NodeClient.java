4c1a96942766deab41ba5b7ad9334f9671c99aff
Adryan N. Reis <88512599+Nasc1mento@users.noreply.github.com>
2023-05-18 10:09:00 -0300

    Update NodeClient.java

--- test/unit/first/question3/NodeClient.java
+++ test/unit/first/question3/NodeClient.java
@@ -1,34 +1,108 @@
 package test.unit.first.question3;
 
-public class NodeClient {
-	
-	
-	private NodeClient next;
-	private Client client;
-	
-	
-	public NodeClient(NodeClient next, Client client) {
-		this.next = next;
-		this.client = client;
-	}
-	
-	public NodeClient(Client client) {
-		this.next = null;
-		this.client = client;
+public class QueueClients {
+
+	private NodeClient first;
+	private NodeClient end;
+	private int size;
+
+	public QueueClients() {
+		first = end = null;
+		this.size = 0;
 	}
 
+	public void enqueue(Client client) {
 
-	public NodeClient getNext() {
-		return next;
+		if (isEmpty()) {
+			first = end = new NodeClient(client);
+			size++;
+			return;
+		}
+
+		if (client.isPriority()) {
+//			first = new NodeClient(first, client);
+//			size++;
+			
+			NodeClient current = first;
+			NodeClient previous = null;
+			
+			while (current != null && current.getClient().isPriority()) {
+				previous = current;
+				current = current.getNext();
+			}
+			
+			
+			NodeClient newClient = new NodeClient(current, client);
+			if (previous == null) {
+				first = newClient;
+			}else {
+				previous.setNext(newClient);
+			}
+			
+		} else {
+			end.setNext(new NodeClient(client));
+			size++;
+		}
 	}
 
+	public Client dequeue() {
 
-	public void setNext(NodeClient next) {
-		this.next = next;
+		if (isEmpty())
+			return null;
+
+		NodeClient temp = first;
+
+		if (first == end) {
+			first = end = null;
+			size--;
+			return temp.getClient();
+		}
+
+		first = first.getNext();
+		size--;
+		return temp.getClient();
+
 	}
 
+	public Client peek() {
+		return this.first.getClient();
+	}
 
-	public Client getClient() {
-		return client;
+	public int size() {
+		return this.size;
+	}
+	
+	public boolean contains(Client client) {
+		if (isEmpty())
+			return false;
+		
+		NodeClient temp = first;
+		
+		while (temp != null) {
+			if (temp.getClient().equals(client))
+				return true;
+			temp = temp.getNext();
+		}
+		
+		return false;
+	}
+
+	public String toString() {
+		String temp = "[";
+		if (isEmpty()) {
+			temp += "]";
+			return temp;
+		}
+		Client aux = first.getClient();
+		while (aux != null) {
+			temp += aux + ", ";
+			aux = aux.getNext();
+		}
+		temp += "]";
+		return temp;
+	}
+
+	public boolean isEmpty() {
+		return this.first == null;
 	}
 }
